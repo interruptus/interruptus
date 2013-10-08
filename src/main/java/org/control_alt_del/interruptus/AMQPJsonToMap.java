bdd09715a9b9c124d317d6db30044a53166301bb
Mark Steele <mark@control-alt-del.org>
2013-10-08 04:22:09 -0400

    added some notes on packet sizes

--- src/main/java/org/control_alt_del/interruptus/AMQPJsonToMap.java
+++ src/main/java/org/control_alt_del/interruptus/AMQPJsonToMap.java
@@ -53,6 +53,14 @@ public class AMQPJsonToMap implements AMQPToObjectCollector
     @Override
     public void collect(AMQPToObjectCollectorContext context)
     {
+        // Max UDP packet size without fragmentation = 1500 bytes (MTU on ethernet without jumbo frames) - IP header (60 bytes max) - UDP header (8 bytes) = 1432 bytes
+        // Ideally if we're trying to squeeze every little bit of performance, we should require clients to do path MTU discovery and encode 
+        // pre-compression length in first 4 bytes of payload. We would also enfore that that UDP packet size after compression be < 1432
+        // This would allow us to size buffers appropriately as well as have the lowest possible amount of TCP/IP header overhead.
+        // See http://tools.ietf.org/html/rfc5405#section-3.2
+        // http://stackoverflow.com/questions/973439/how-to-set-the-dont-fragment-df-flag-on-a-socket
+        // Should do some testing on alternate compression algorithms to see what gives us best bang for the buck. Gut feeling is that zlib reduces by > 50%
+        // as json encoded metrics have lots of repetition.
         byte[] input        = context.getBytes();
 
         String json  = getJsonString(input);
@@ -79,7 +87,7 @@ public class AMQPJsonToMap implements AMQPToObjectCollector
     private String getJsonString(byte[] input)
     {
 	if ((char) input[0] != '{') { // Very cheezy....
-          byte[] restored = new byte[65507];  // Max UDP packet size
+          byte[] restored = new byte[65507];  // Max UDP packet size, worst case scenario until we get client to support encoding payload uncmopressed length.
           try {
               Inflater decompresser = new Inflater();
               decompresser.setInput(input,0,input.length);
