dbbb90c3742b996feb48dcfac52a40bb81eef43a
fabios <fabios@nationalfibre.net>
2014-05-30 15:01:03 -0400

    add example to README

--- README.md
+++ README.md
@@ -45,6 +45,54 @@ Authors
  Fabio "Fantastico" B. Silva <fabio.bat.silva@gmail.com>
 
 
+Configuration example
+======================
+
+The following example configures an event type in Interruptus and uses an AMQP inbound data flow to read AMQP messages.
+
+
+```
+curl -X POST 'http://localhost:8080/api/type' -H "Content-Type:application/json" -d '{
+    "name":"CollectdMetric",
+    "properties":{
+      "plugin":"string",
+      "plugin_instance":"string",
+      "type":"string",
+      "type_instance":"string",
+      "datacenter":"string",
+      "time":"long",
+      "value":"double",
+      "name":"string",
+      "host":"string"
+    }
+}'
+
+curl -X POST 'http://localhost:8080/api/statement' -H "Content-Type:application/json" -d '{
+    "name":"eventlogdebug",
+    "query":"SELECT * FROM CollectdMetric WHERE host = \"mq01.ss\"",
+    "debug":true,
+    "started":true
+}'
+
+curl -X POST 'http://localhost:8080/api/flow' -H "Content-Type:application/json" -d '{
+  "name":"EventsIn",
+  "started":true,
+  "query":"
+    create dataflow EventsIn AMQPSource -> EventsIn<CollectdMetric>
+    {
+        collector:    {class: \"org.cad.interruptus.AMQPJsonToMap\"},
+        host:         \"localhost\",
+        exchange:     \"collectd_metrics\",
+        port:         5672,
+        username:     \"guest\",
+        password:     \"guest\",
+        routingKey:   \"#\",
+        logMessages:  true
+    } EventBusSink(EventsIn){}"
+}'
+
+```
+
 API USAGE
 =========
 
--- bin/fixtures/test-collectd-metrics.sh
+++ bin/fixtures/test-collectd-metrics.sh
@@ -1,33 +1,69 @@
-curl -X POST -H "Content-Type:application/json" -d @- http://localhost:8080/api/type <<EOF | python -m json.tool
-{
-  "name":"CollectdMetric",
-  "properties":{
-    "plugin":"string",
-    "plugin_instance":"string",
-    "type":"string",
-    "type_instance":"string",
-    "datacenter":"string",
-    "time":"long",
-    "value":"double",
-    "name":"string",
-    "host":"string"
-  }
-}
-EOF
+curl -X POST 'http://localhost:8080/api/type' -H "Content-Type:application/json" -d '{
+    "name":"CollectdMetric",
+    "properties":{
+      "plugin":"string",
+      "plugin_instance":"string",
+      "type":"string",
+      "type_instance":"string",
+      "datacenter":"string",
+      "time":"long",
+      "value":"double",
+      "name":"string",
+      "host":"string"
+    }
+}'
 
 # {"type":"CollectdMetric","body":{"plugin":"disk","plugin_instance":"disk_sdd1","datacenter":"east_coast","time":"1399577466","host":"mq01.ss","name":"usage","value":"1234591"}}
 
-curl -X POST -H "Content-Type:application/json" -d @- http://localhost:8080/api/statement <<EOF | python -m json.tool
-{"name": "eventlogdebug", "query":"SELECT * FROM CollectdMetric WHERE host = 'mq01.ss'", "debug": true, "started": true}
-EOF
+curl -X POST 'http://localhost:8080/api/statement' -H "Content-Type:application/json" -d '{
+    "name":"eventlogdebug",
+    "query":"SELECT * FROM CollectdMetric WHERE host = \"mq01.ss\"",
+    "debug":true,
+    "started":true
+}'
 
-curl -X POST -H "Content-Type:application/json" -d @- http://localhost:8080/api/flow <<EOF | python -m json.tool
-{"name": "EventsOut", "started": true, "masterOnly":true, "query":"create dataflow EventsOut EventBusSource -> outstream<CollectdMetric> {} AMQPSink(outstream) { host: 'localhost', exchange: 'alerts', queueName: 'alerts', username: 'guest', password: 'guest', routingKey: '#', declareAutoDelete: false, declareDurable: true, collector: {class: 'org.cad.interruptus.EventToAMQP'},logMessages: true}"}
-EOF
+curl -X POST 'http://localhost:8080/api/flow' -H "Content-Type:application/json" -d '{
+  "name":"EventsIn",
+  "started":true,
+  "query":"
+    create dataflow EventsIn AMQPSource -> EventsIn<CollectdMetric>
+    {
+        collector:    {class: \"org.cad.interruptus.AMQPJsonToMap\"},
+        host:         \"localhost\",
+        exchange:     \"collectd_metrics\",
+        port:         5672,
+        username:     \"guest\",
+        password:     \"guest\",
+        routingKey:   \"#\",
+        logMessages:  true
+    } EventBusSink(EventsIn){}"
+}'
 
-curl -X POST -H "Content-Type:application/json" -d @- http://localhost:8080/api/flow <<EOF | python -m json.tool
-{"name": "EventsIn", "started": true, "query":"create dataflow EventsIn AMQPSource -> EventsIn<CollectdMetric> {  host: 'localhost',  exchange: 'collectd_metrics', port: 5672, username: 'guest',  password: 'guest',  routingKey: '#', collector: {class: 'org.cad.interruptus.AMQPJsonToMap'}, logMessages: true  } EventBusSink(EventsIn){}"}
-EOF
+curl -X POST 'http://localhost:8080/api/flow' -H "Content-Type:application/json" -d '{
+  "name":"EventsOut",
+  "masterOnly":true,
+  "started":true,
+  "query":"
+    create dataflow EventsOut
+      EventBusSource -> outstream<CollectdMetric>
+      {
+
+      }
+      AMQPSink(outstream)
+      {
+        collector: {class: \"org.cad.interruptus.EventToAMQP\"},
+        host:               \"localhost\",
+        exchange:           \"alerts\",
+        queueName:          \"alerts\",
+        username:           \"guest\",
+        password:           \"guest\",
+        routingKey:         \"#\",
+        declareAutoDelete:  false,
+        declareDurable:     true,
+        logMessages:        true
+    }
+  "
+}'
 
 # curl -X POST http://localhost:8080/api/flow/EventsIn/start | python -m json.tool
 # curl -X POST http://localhost:8080/api/flow/EventsOut/start | python -m json.tool
